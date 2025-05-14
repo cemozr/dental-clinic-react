127eef008e4ca57a70e36c23977d5396e0ff33c8
Cem <98841937+cemozr@users.noreply.github.com>
2025-05-14 18:19:37 +0300

    hamburger menu logout button fix

--- src/components/header/Navigation.tsx
+++ src/components/header/Navigation.tsx
@@ -1,8 +1,15 @@
+import { useDispatch } from "react-redux";
 import useAuth from "../../hooks/useAuth";
 import Button from "../UI/Button";
+import { AppDispatch } from "../../states/store";
+import { logout } from "../../states/authSlice";
+import { useNavigate } from "react-router-dom";
+import { GiExitDoor } from "react-icons/gi";
 
 export default function Navigation() {
   const { userStatus } = useAuth();
+  const dispatch: AppDispatch = useDispatch();
+  const navigate = useNavigate();
   const menus: { name: string; to: string }[] = [
     { name: "Ana Sayfa", to: "/" },
     { name: "Hizmetler", to: "/services" },
@@ -12,6 +19,13 @@ export default function Navigation() {
       ? { name: "Yönetim", to: "/admin" }
       : { name: "Yönetim", to: "/auth" },
   ];
+
+  const handleSignOut = () => {
+    dispatch(logout()).then(
+      (res) => res.meta.requestStatus === "fulfilled" && navigate("/"),
+    );
+  };
+
   return (
     <>
       {menus.map((menu, i) => {
@@ -23,10 +37,15 @@ export default function Navigation() {
           </li>
         );
       })}
-      <li className="lg:hidden">
+      <li className="flex flex-col items-center gap-1 lg:hidden">
         <Button el="colored-link" to="/appointment">
           Randevu Al
         </Button>
+        {userStatus === "signed in" && (
+          <Button el="icon-button" onClick={handleSignOut}>
+            <GiExitDoor />
+          </Button>
+        )}
       </li>
     </>
   );
